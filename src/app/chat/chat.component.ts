232c596db711d9ae79c52441cbe2f59fb98c4a53
stabbaa <siham.tabbaa@etu.unice.fr>
2024-06-13 13:31:13 +0200

    Slash image OK (mais pas avec le GUI)

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -3,7 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { OpenaiService } from '../services/openai.service';
-import { environment } from '../environment/environment';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+export interface Message {
+  sender: string;
+  content: string | SafeHtml;
+}
 
 @Component({
   selector: 'app-chat',
@@ -20,31 +25,78 @@ import { environment } from '../environment/environment';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
+
 export class ChatComponent {
-  messages = [{ sender: 'MiageGPT', content: 'Bienvenue sur MiageGPT!' }];
+  messages: Message[] = [{ sender: 'MiageGPT', content: 'Bienvenue sur MiageGPT!' }];
   userInput = '';
   selectedModel = 'gpt-3.5-turbo';
+  selectedDalleModel = 'dall-e-2';
 
-  constructor(private openaiService: OpenaiService) { }
+  constructor(private openaiService: OpenaiService, private sanitizer: DomSanitizer) { }
 
   sendMessage() {
-    //console.log(openaiApiKey);
     const trimmedInput = this.userInput.trim();
-    if (trimmedInput) {
-      this.messages.push({ sender: 'User', content: trimmedInput });
-
-      this.openaiService.sendMessage(trimmedInput, this.selectedModel).subscribe({
-        next: (response) => {
-          console.log('Response from OpenAI:', response);
-          const botResponse = response.choices[0].message.content;
-          this.messages.push({ sender: 'MiageGPT', content: botResponse });
-        },
-        error: (err) => {
-          console.error('Error when calling OpenAI:', err);
-          this.messages.push({ sender: 'MiageGPT', content: 'Erreur lors de la connexion à OpenAI.' });
-        }
-      });
-      this.userInput = '';
+    this.userInput = '';
+    if (!trimmedInput) return;
+    
+    if (trimmedInput.startsWith('/')) {
+      this.handleCommand(trimmedInput);
+    } else {
+      this.sendTextMessage(trimmedInput);
     }
+  }
+
+  sendTextMessage(text: string) {
+    this.messages.push({ sender: 'User', content: text });
+    this.openaiService.sendMessage(text, this.selectedModel).subscribe({
+      next: (response) => {
+        const botResponse = response.choices[0].message.content;
+        this.messages.push({ sender: 'MiageGPT', content: botResponse });
+      },
+      error: (err) => {
+        console.error('Error when calling OpenAI:', err);
+        this.messages.push({ sender: 'MiageGPT', content: 'Erreur lors de la connexion à OpenAI.' });
+      }
+    });
+  }
+
+  handleCommand(command: string) {
+    const [commandKey, ...args] = command.split(' ');
+    const description = args.join(' ');
+    
+    switch (commandKey) {
+      case '/image':
+        const text = "Génère l'image : " + description;
+        this.messages.push({ sender: 'User', content: text });
+        this.handleImageCommand(description);
+        break;
+      default:
+        this.messages.push({ sender: 'System', content: "Commande inconnue. Essayez de nouveau!" });
+    }
+  }
+
+  handleImageCommand(description: string) {
+    if (!description) {
+      this.messages.push({ sender: 'System', content: "Veuillez fournir une description pour l'image." });
+      return;
+    }
+
+    console.log('Demande d\'image:', description);
+
+    this.openaiService.sendImageRequest(description, this.selectedDalleModel).subscribe({
+      next: (response) => {
+        console.log('Réponse de l\'API pour l\'image:', response);
+
+        const imageUrl = response.data[0].url;
+        console.log('URL de l\'image:', imageUrl);
+
+        const imageHtml = this.sanitizer.bypassSecurityTrustHtml(`<img src="${imageUrl}" alt="Generated Image" style="max-width:100%; height:auto;">`);
+        this.messages.push({ sender: 'MiageGPT', content: imageHtml as string });
+      },
+      error: (err) => {
+        console.error('Error when calling OpenAI:', err);
+        this.messages.push({ sender: 'MiageGPT', content: 'Erreur lors de la connexion à OpenAI.' });
+      }
+    });
   }
 }
--- src/app/services/openai.service.ts
+++ src/app/services/openai.service.ts
@@ -6,24 +6,39 @@ import { environment } from '../environment/environment';
 @Injectable({
   providedIn: 'root'
 })
+
 export class OpenaiService {
-  private apiURL = 'https://api.openai.com/v1/chat/completions';
+  private textAPIURL = 'https://api.openai.com/v1/chat/completions';
+  private imageAPIURL = 'https://api.openai.com/v1/images/generations';
 
   constructor(private http: HttpClient) { }
 
   sendMessage(prompt: string, model: string = "gpt-3.5-turbo"): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${environment.openaiApiKey}`
-    });
-
+    const headers = this.createHeaders();
     const body = {
       model: model,
       messages: [{ role: "user", content: prompt }]
     };
 
-    console.log('Sending request to OpenAI:', body);
+    return this.http.post(this.textAPIURL, body, { headers });
+  }
 
-    return this.http.post(this.apiURL, body, { headers });
+  sendImageRequest(description: string, model: string = "dall-e-2"): Observable<any> {
+    const headers = this.createHeaders();
+    const body = {
+      prompt: description,
+      model: model,
+      n: 1,
+      size: "1024x1024"
+    };
+
+    return this.http.post(this.imageAPIURL, body, { headers });
+  }
+
+  private createHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${environment.openaiApiKey}`
+    });
   }
 }
